e27756ce73c859e8f62f10269ea07707316dca65
AvantikaBnsl <148711429+AvantikaBnsl@users.noreply.github.com>
2024-04-09 22:51:01 +0530

    Update seller.C

--- seller.C
+++ seller.C
@@ -1,1 +1,15 @@
+#include <iostream> 
+#include <user.h> 
 
+class Seller : public User
+{
+private: 
+int num_sales; 
+
+public: 
+Seller(); 
+Seller(string name, string id); 
+setItemDetails(); // seller will update details about the product they are seller 
+setItemDetails(char archtype, string name, int quantity, float price); 
+
+};
